41855a45a7fa5260dc499f62ad35435556c459ba
Zac <zac.minidude140@Gmail.com>
2023-09-04 10:25:43 -0600

    Added user Emily and anyone not allowed

--- SayMyNameAgain/SayMyNameAgain.vb
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -12,8 +12,8 @@ Module SayMyNameAgain
 
     Sub Main()
         'TODO 
-        '[]Modify so that only select users get a reply
-        '[]Identify user "Emily"
+        '[~]Modify so that only select users get a reply
+        '[~]Identify user "Emily"
         '[]Identify user "Joe"
         '[]Remark on users case sensitivity 
         '[]Idenity if the user has my name
@@ -27,6 +27,12 @@ Module SayMyNameAgain
         name = Console.ReadLine()
 
         'Determine response here
+        Select Case name
+            Case = "Emily"
+                Console.WriteLine("Why Hello, Emily." & vbLf & "Have a great day." & vbLf)
+            Case Else
+                Console.WriteLine($"Sorry, no one by the name: {name} is allowed access." & vbLf)
+        End Select
 
         'console prompts user to close program
         Console.WriteLine("Please push enter to quit program.")
