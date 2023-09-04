967ec570671443bd8f0b316836c521d86ac9f744
Zac <zac.minidude140@Gmail.com>
2023-09-04 09:15:23 -0600

    Add project files.

---
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -0,0 +1,17 @@
+﻿'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'Say My Name Again
+'https://github.com/Minidude140
+
+
+Option Explicit On
+Option Strict On
+
+Module SayMyNameAgain
+
+    Sub Main()
+
+    End Sub
+
+End Module
