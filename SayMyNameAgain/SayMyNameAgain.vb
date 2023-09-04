7ada963d1413e45126cb2bf34b597aaa4515012c
Zac <zac.minidude140@Gmail.com>
2023-09-04 10:56:31 -0600

    Added Loop and Quit Functionality

--- SayMyNameAgain/SayMyNameAgain.vb
+++ SayMyNameAgain/SayMyNameAgain.vb
@@ -17,39 +17,56 @@ Module SayMyNameAgain
         '[~]Identify user "Joe"
         '[~]Remark on users case sensitivity 
         '[]Idenity if the user has my name
-        '[]Add loop and Quit functionality
+        '[~]Add loop and Quit functionality
+        '[]Add console clear when repeating program
 
         Dim name As String
+        Dim exitFlag As Boolean = False
+        Dim cont As String
 
-        'Console asks user for thier name
-        Console.WriteLine("Hello, What is your name?")
+        Do
+            'Console asks user for thier name
+            Console.WriteLine("Hello, What is your name?")
 
-        'console sets variable "name" to user input
-        name = Console.ReadLine()
+            'console sets variable "name" to user input
+            name = Console.ReadLine()
 
-        'Determine response here
-        Select Case name
+            'Determine response here
+            Select Case name
             'User Emily
-            Case = "Emily"
-                Console.WriteLine("Why Hello, Emily." & vbLf & "Have a great day." & vbLf)
+                Case = "Emily"
+                    Console.WriteLine("Why Hello, Emily." & vbLf & "Have a great day." & vbLf)
 
             'User Joe 
-            Case = "Joe"
-                Console.WriteLine("Good to see you again, Joe." & vbLf)
+                Case = "Joe"
+                    Console.WriteLine("Good to see you again, Joe." & vbLf)
 
             'Someone didn't use Caps
-            Case = "emily"
-                Console.WriteLine("You should try and capitalize that E, miss Emily!" & vbLf)
-            Case = "joe"
-                Console.WriteLine("Come on Joe, capitalize that J!" & vbLf)
+                Case = "emily"
+                    Console.WriteLine("You should try and capitalize that E, miss Emily!" & vbLf)
+                Case = "joe"
+                    Console.WriteLine("Come on Joe, capitalize that J!" & vbLf)
 
-                'User Access Denied
-            Case Else
-                Console.WriteLine($"Sorry, no one by the name: {name} is allowed access." & vbLf)
-        End Select
+                    'User Access Denied
+                Case Else
+                    Console.WriteLine($"Sorry, no one by the name: {name} is allowed access." & vbLf)
+            End Select
 
-        'console prompts user to close program
-        Console.WriteLine("Please push enter to quit program.")
+            'Ask user if they wnt to wuit or continue
+            Console.WriteLine("Continue?" & vbLf & "(Press Q to quit)" & vbLf)
+            cont = Console.ReadLine()
+
+            'Idenity exit loop or not
+            If cont = ("Q") Then
+                exitFlag = True
+            ElseIf cont = ("q") Then
+                exitFlag = True
+            Else
+                exitFlag = False
+            End If
+
+        Loop Until exitFlag = True
+
         Console.Read()
 
     End Sub
